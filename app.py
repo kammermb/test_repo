4b2a7bc2364bfe961a8081a37775da29f25b3b63
kammermb <148339363+kammermb@users.noreply.github.com>
2023-11-16 20:55:19 +0100

    feature 2

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
 from Feature1 import return_even
+from Feature2 import return_odd
 
 org_list = [i for i in range(10)]
 
 even_list = return_even(org_list)
 
+odd_list = return_odd(org_list)
+
 st.write("Hello World")
 
 st.write(even_list)
+st.write(odd_list)
